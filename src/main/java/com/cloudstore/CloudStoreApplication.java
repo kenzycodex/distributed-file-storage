86a84c57c5d982661cc7abedc327f1d617e43dc7
kenzycodex <kenzycodex@gmail.com>
2025-02-03 13:35:14 +0100

    COMPLETE PROJECT STRUCTURE

---
+++ src/main/java/com/cloudstore/CloudStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cloudstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class CloudStoreApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
